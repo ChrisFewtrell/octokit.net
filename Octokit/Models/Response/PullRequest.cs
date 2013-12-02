46d462001565975e0a69f9daa7e9b93790bf8225
chrisfewtrell <chris.fewtrell@grantadesign.com>
2013-12-02 08:11:15 +0000

    Added pull request properties

--- Octokit/Models/Response/PullRequest.cs
+++ Octokit/Models/Response/PullRequest.cs
@@ -2,10 +2,39 @@
 
 namespace Octokit.Response
 {
-    public class PullRequest
+    public class PullRequest : PullRequestDetails
     {
-        public Uri HtmlUrl { get; set; }
-        public Uri DiffUrl { get; set; }
-        public Uri PatchUrl { get; set; }
+        public Uri IssueUri { get; set; }
+        public Uri StatusesUri { get; set; }
+        public int Number { get; set; }
+        public PullRequestState State { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public DateTimeOffset CreatedAt { get; set; }
+        public DateTimeOffset UpdatedAt { get; set; }
+        public DateTimeOffset? ClosedAt { get; set; }
+        public DateTimeOffset? MergedAt { get; set; }
+        public Branch Head { get; set; }
+        public Branch Base { get; set; }
+        public User User { get; set; }
+        public string MergeCommitSha { get; set; }
+        public bool Merged { get; set; }
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1704:IdentifiersShouldBeSpelledCorrectly", MessageId = "Mergeable")]
+        public bool Mergeable { get; set; }
+        public User MergedBy { get; set; }
+        public int Comments { get; set; }
+        public int Commits { get; set; }
+        public int Additions { get; set; }
+        public int Deletions { get; set; }
+        public int ChangedFiles { get; set; }
+    }
+
+    public class Branch
+    {
+        public string Label { get; set; }
+        public string Ref { get; set; }
+        public string Sha { get; set; }
+        public User User { get; set; }
+        public Repository Repo { get; set; }
     }
 }
